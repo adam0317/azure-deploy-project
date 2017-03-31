71ea47ecb47af64bef59631951f4038d18e01e95
adam0317 <adam@offthehighdive.com>
2017-03-30 19:29:10 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express         = require('express');
+var path            = require('path');
+var jwt             = require('jsonwebtoken');
+var massive         = require('massive');
+var session         = require('express-sessions');
+var favicon         = require('serve-favicon');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+
+var app             = express();
+var port = process.env.PORT || 8081;
+
+
+app.use(bodyParser.json());
+
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/client/index.html');
+// });
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+})
