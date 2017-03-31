311ed9415800c425a251797a3baf1cb6869fac1b
adam0317 <adam@offthehighdive.com>
2017-03-30 20:46:17 -0600

    added NODE_ENV

--- server.js
+++ server.js
@@ -8,22 +8,25 @@ var favicon         = require('serve-favicon');
 var logger          = require('morgan');
 var cookieParser    = require('cookie-parser');
 var bodyParser      = require('body-parser');
-// var massiveInstance = massive.connectSync({
-//   connectionString:   process.env.connectionString || config.connectionString
-// });
+if (process.env.NODE_ENV) {
+	var massiveInstance = massive.connectSync({
+  connectionString:   process.env.connectionString
+});
+}
+
 var app             = express();
 var port = process.env.port || 8081;
 
 app.use(bodyParser.json());
 
-//app.set('db', massiveInstance);
+app.set('db', massiveInstance);
 module.exports      = app;
 var apiRoutes       = require('./api/apiRoutes.js');
-//var dbController    = require('./api/dbController.js');
+var dbController    = require('./api/dbController.js');
 
 app.use('/scripts', express.static(__dirname + '/client/node_modules'));
 app.use('/styles', express.static(__dirname + '/client/node_modules'));
-//app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 
 app.use(express.static(path.resolve(__dirname, 'client')));
