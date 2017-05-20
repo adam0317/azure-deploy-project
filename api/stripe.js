f06656b145be25d26a81ce4f2b489183a42be515
adam0317 <adam@offthehighdive.com>
2017-05-20 11:51:07 -0600

    added $location

--- api/stripe.js
+++ api/stripe.js
@@ -1,27 +1,22 @@
 
 
+if (!process.env.node_env) {
+	var config = require('../config.js');
+	var stripe = require("stripe")(
+		config.stripeKey
+	);
+}
+if (process.env.node_env) {
+	// Add to Azure Config File
+	var keySecret = process.env.SECRET_KEY;
+	var stripe = require("stripe")(keySecret);
+}
+
+
 
 exports.charge = function (req, res, next) {
-if (!process.env.node_env) {
-		var config = require('../config.js');
-		var stripe = require("stripe")(
-			config.stripeKey
-		);
-	} 
-	if (process.env.node_env) {
-		// Add to Azure Config File
-		const keySecret = process.env.SECRET_KEY;
-		const stripe = require("stripe")(keySecret);
-			}
-	
 
-	// var config = require('../config.js');
-	// var stripe = require("stripe")(
-	// 	config.stripeKey
-	// 	);
-	// console.log('charge fired', req.body)
 	var amount = req.body.amount;
-
 	stripe.customers.create({
 		email: req.body.email,
 		source: req.body.id
@@ -35,7 +30,6 @@ if (!process.env.node_env) {
 			})).then(function (response) {
 				res.send(response);
 			})
-
 };
 
 
--- client/checkout/checkoutService.js
+++ client/checkout/checkoutService.js
@@ -1,12 +1,12 @@
 (function () {
 	'use strict';
-	angular.module('app').service('checkoutService', function ($http, $q, userService) {
-
+	angular.module('app').service('checkoutService', function ($http, $q, userService, $location) {
+		var host = new window.URL($location.absUrl()).origin;
 	this.chargeCard = function (token, amount) {
 		console.log('amoutn', amount);
 		token.amount = amount;
 		var defer = $q.defer();
-		$http.post('/api/chargeCard', token).then(function (result) {
+		$http.post(host + '/api/chargeCard', token).then(function (result) {
 			console.log('api.get fired', result);
 			defer.resolve(result);
 		})
